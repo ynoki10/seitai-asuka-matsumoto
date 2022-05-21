c51d0554cf67250d0f88b6cf3d0e6f9f72283a38
ynoki <yoshinoki63@gmail.com>
2022-05-21 22:28:28 +0900

    ESlint + Prettier導入

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,7 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+  return <h1 className=" text-3xl font-bold underline">Hello world!</h1>;
+};
 
-export default Home
+export default Home;
