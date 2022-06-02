c45876aae465683f191ac95ad2018bae33033b90
ynoki <yoshinoki63@gmail.com>
2022-06-02 17:40:56 +0900

    tailwind設定

---
+++ src/pages/_document.tsx
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500;700&family=Zen+Maru+Gothic:wght@500&display=swap" rel="stylesheet"></link>
+      </Head>
+      <body className="font-sans">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,27 @@
 module.exports = {
   content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        white: '#FFFFF0',
+        black: '#333333',
+        green01: '#1C7C54',
+        green02: '#449365',
+        green03: '#629460',
+        green04: '#7FB069',
+        green05: '#6BAA75',
+        pale01: '#BBD8C8',
+        pale02: '#C7DFCD',
+        pale03: '#D0DFCC',
+        pale04: '#D9E7CF',
+        pale05: '#D3E5D2',
+        accent: '#E8AA14',
+      },
+      fontFamily: {
+        sans: 'Noto Sans JP, sans-serif',
+        maru: 'Zen Maru Gothic, sans-serif',
+      },
+    },
   },
   plugins: [],
 };
