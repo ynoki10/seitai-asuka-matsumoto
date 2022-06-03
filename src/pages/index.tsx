a03feb27acc14d4d332688644c423427d5e46d63
ynoki <yoshinoki63@gmail.com>
2022-06-03 16:18:01 +0900

    SP用ヘッダー作成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next';
 
-const Home: NextPage = () => {
-  return <h1 className=" text-3xl font-bold underline">Hello world!</h1>;
+const HomePage: NextPage = () => {
+  return <></>;
 };
 
-export default Home;
+export default HomePage;
