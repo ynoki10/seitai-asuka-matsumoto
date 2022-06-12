fdc3daa4dbf2b2c8f6a0251b2c0145f220652079
ynoki <yoshinoki63@gmail.com>
2022-06-13 08:15:28 +0900

    PCフッター作成

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -45,88 +45,109 @@ const footerMenuItems = [
 const Footer = () => {
   return (
     <>
-      <footer className={'py-10 mt-[100px] text-white bg-green03'}>
-        <Inner>
-          <div className={'mx-auto max-w-[225px] md:max-w-[280px]'}>
-            <Link href="/">
-              <a>
-                <Image
-                  src={'/assets/images/logo-txt-white.svg'}
-                  alt="慢性頭痛・肩こり専門 整体院あす香"
-                  width={280}
-                  height={78}
-                />
-              </a>
-            </Link>
-          </div>
-          <p className={'mt-4 text-sm font-normal tracking-wider leading-relaxed text-center'}>
-            〒390-1243
-            <br />
-            長野県松本市神林5108-8
-            <br />
-            【営業時間】10:00~19:00 不定休
-          </p>
-          <div className={'mt-6'}>
-            <p className={'text-xs tracking-wider  text-center'}>
-              ご予約・ご相談 まずはお気軽にご連絡ください！※
-            </p>
-            <a
-              href="tel:070-1586-4141"
-              className={
-                'flex gap-x-2 justify-center items-center py-1.5 mt-2 bg-accent rounded-md'
-              }
-            >
-              <Image src="/assets/images/ico-tel-white.svg" alt="" width="30" height="30" />
-              <span className={'text-[26px] tracking-wider'}>070-1586-4141</span>
-            </a>
-            <Link href={'/contact'}>
-              <a
+      <footer className={'py-10 mt-[100px] text-white bg-green03 md:pt-16'}>
+        <Inner className={'md:relative'}>
+          <div className={'md:flex md:gap-x-16'}>
+            <div className={'shrink-0 md:max-w-[330px] lg:max-w-[400px]'}>
+              <div className={'mx-auto max-w-[225px] md:mx-0 md:max-w-[280px]'}>
+                <Link href="/">
+                  <a>
+                    <Image
+                      src={'/assets/images/logo-txt-white.svg'}
+                      alt="慢性頭痛・肩こり専門 整体院あす香"
+                      width={280}
+                      height={78}
+                    />
+                  </a>
+                </Link>
+              </div>
+              <p
                 className={
-                  'flex gap-x-2 justify-center items-center py-1.5 mt-3 text-white bg-green01 rounded-md'
+                  'mt-4 text-sm font-normal tracking-wider leading-relaxed text-center md:text-left lg:text-[16px]'
                 }
               >
-                <Image src="/assets/images/ico-mail.svg" alt="" width={24} height={19} />
-                <span className={'tracking-wider'}>ご予約・お問い合わせフォーム</span>
-              </a>
-            </Link>
-            <ul className={'pl-2 mt-2 font-normal'}>
-              <li
-                className={
-                  'before:block before:absolute relative before:top-0 before:left-[-1em] text-xxs before:content-["※"]'
-                }
-              >
-                施術中は電話に出られないこともございます。
-              </li>
-              <li
-                className={
-                  'before:block before:absolute relative before:top-0 before:left-[-1em] text-xxs before:content-["※"]'
-                }
-              >
-                当院は完全予約制となっております。施術をご希望の方は電話かメールにてあらかじめご予約ください。
-              </li>
-            </ul>
-            <div className={'mt-8 h-60'}>
-              <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3220.4241757990085!2d137.92659903033748!3d36.18056454633208!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x601d0f7941b2d531%3A0x8390612228059a08!2z5pW05L2T6ZmiIOOBguOBmemmmQ!5e0!3m2!1sja!2sjp!4v1655034934087!5m2!1sja!2sjp"
-                width="100%"
-                height="100%"
-                allowFullScreen
-                loading="lazy"
-                referrerPolicy="no-referrer-when-downgrade"
-              ></iframe>
+                〒390-1243
+                <br />
+                長野県松本市神林5108-8
+                <br />
+                【営業時間】10:00~19:00 不定休
+              </p>
+              <div className={'mt-6'}>
+                <p className={'text-xs tracking-wider  text-center'}>
+                  ご予約・ご相談 まずはお気軽にご連絡ください！※
+                </p>
+                <a
+                  href="tel:070-1586-4141"
+                  className={
+                    'flex gap-x-2 justify-center items-center py-1.5 mt-2 bg-accent rounded-md'
+                  }
+                >
+                  <Image src="/assets/images/ico-tel-white.svg" alt="" width="30" height="30" />
+                  <span className={'text-[26px] tracking-wider'}>070-1586-4141</span>
+                </a>
+                <Link href={'/contact'}>
+                  <a
+                    className={
+                      'flex gap-x-2 justify-center items-center py-1.5 mt-3 text-white bg-green01 rounded-md'
+                    }
+                  >
+                    <Image src="/assets/images/ico-mail.svg" alt="" width={24} height={19} />
+                    <span className={'tracking-wider'}>ご予約・お問い合わせフォーム</span>
+                  </a>
+                </Link>
+                <ul className={'pl-2 mt-2 font-normal'}>
+                  <li
+                    className={
+                      'before:block before:absolute relative before:top-0 before:left-[-1em] text-xxs tracking-wider before:content-["※"] lg:text-[12px] lg:leading-normal'
+                    }
+                  >
+                    施術中は電話に出られないこともございます。
+                  </li>
+                  <li
+                    className={
+                      'before:block before:absolute relative before:top-0 before:left-[-1em] text-xxs tracking-wider before:content-["※"] lg:text-[12px] lg:leading-normal'
+                    }
+                  >
+                    当院は完全予約制となっております。施術をご希望の方は電話かメールにてあらかじめご予約ください。
+                  </li>
+                </ul>
+              </div>
             </div>
-            <nav className={'flex flex-col gap-y-6 items-center mt-8'}>
-              {footerMenuItems.map((item) => {
-                return (
-                  <Link href={item.path} key={item.text}>
-                    <a className={'font-normal'}>{item.text}</a>
-                  </Link>
-                );
-              })}
-            </nav>
-            <small className={'block mt-10 text-xxs text-center'}>
-              © 整体院あす香 All Rights Reserved.
-            </small>
+            <div className={'shrink w-full'}>
+              <div className={'mt-8 h-60 md:mt-0'}>
+                <iframe
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3220.4241757990085!2d137.92659903033748!3d36.18056454633208!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x601d0f7941b2d531%3A0x8390612228059a08!2z5pW05L2T6ZmiIOOBguOBmemmmQ!5e0!3m2!1sja!2sjp!4v1655034934087!5m2!1sja!2sjp"
+                  width="100%"
+                  height="100%"
+                  allowFullScreen
+                  loading="lazy"
+                  referrerPolicy="no-referrer-when-downgrade"
+                ></iframe>
+              </div>
+              <nav
+                className={
+                  'flex flex-col gap-y-6 items-center mt-8 md:flex-row md:flex-wrap md:gap-y-4'
+                }
+              >
+                {footerMenuItems.map((item) => {
+                  return (
+                    <Link href={item.path} key={item.text}>
+                      <a className={'font-normal md:mr-[1.5em] md:text-sm lg:text-base'}>
+                        {item.text}
+                      </a>
+                    </Link>
+                  );
+                })}
+              </nav>
+            </div>
+          </div>
+          <small className={'block mt-10 text-xxs text-center md:mt-16'}>
+            © 整体院あす香 All Rights Reserved.
+          </small>
+          <div
+            className={'hidden absolute right-[40px] bottom-[-20px] w-[19%] max-w-[244px] md:block'}
+          >
+            <Image src={'/assets/images/footer-clover.svg'} alt="" width={244} height={182} />
           </div>
         </Inner>
       </footer>
