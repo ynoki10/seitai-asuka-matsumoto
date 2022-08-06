a8a12cd527e8f59185c2c936c59b28704fe410a9
ynoki10 <sakanafront@gmail.com>
2022-08-06 12:19:00 +0900

    デプロイ先のURLを取得してOGP,faviconに設定

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,6 +2,8 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 import { mobileHeaderHeight } from '@/variables/headerVars';
 
+const url = process.env.VERCEL_URL || 'localhost:3000';
+
 export default function Document() {
   return (
     <Html lang="ja">
@@ -11,7 +13,7 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500;700&family=Zen+Maru+Gothic:wght@500&display=swap"
           rel="stylesheet"
         ></link>
-        <link rel="icon" href="https://asuka-matsumoto.com/favicon.ico"></link>
+        <link rel="icon" href={`https://${url}/favicon.ico`}></link>
         <style>
           {`:root {
             --mobile-header-height: ${mobileHeaderHeight};
@@ -19,7 +21,7 @@ export default function Document() {
             --z-index-mobile-menu: 200;
           }`}
         </style>
-        <meta property="og:image" content="https://asuka-matsumoto.com/ogp.png" />
+        <meta property="og:image" content={`https://${url}/ogp.png`} />
         <meta property="og:locale" content="ja_JP" />
         <meta property="og:site_name" content="整体院あす香" />
         <meta property="og:type" content="article" />
