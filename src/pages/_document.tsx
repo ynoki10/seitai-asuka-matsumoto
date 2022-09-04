48351f451ccc88888d0ac1557503dba106e4d045
ynoki10 <sakanafront@gmail.com>
2022-09-04 12:00:22 +0900

    noindex解除

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,7 +8,6 @@ export default function Document() {
   return (
     <Html lang="ja">
       <Head>
-        <meta name="robots" content="noindex , nofollow" />
         <link
           href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500;700&family=Zen+Maru+Gothic:wght@500&display=swap"
           rel="stylesheet"
